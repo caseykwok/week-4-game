f8d1ecb496bbf05c9d8e8dcba119ca5d1451be01
Casey Kwok <casey.kwok@harborx.com>
2017-06-19 10:53:14 -0700

    Add greetings and handling for when defender is not empty

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
 
+	function greetings() {
+		alert("Welcome to the Star Wars RPG Game. Please choose your character.");
+	}
+
 	function resetCharacters() {
 		$("#luke_skywalker").attr("name", "Luke Skywalker");
 		$("#luke_skywalker").attr("health_points", 100);
@@ -37,27 +41,20 @@ $(document).ready(function () {
 		$("#obi_wan.characters").appendTo(".main_menu");
 		$("#darth_sidious.characters").appendTo(".main_menu");
 		$("#darth_maul.characters").appendTo(".main_menu");
-		// $(".your_character .characters").appendTo(".main_menu");
-		// $(".graveyard .characters").appendTo(".main_menu");
-		// $(".enemies_available .characters").appendTo(".main_menu");
-		// $(".defender .characters").appendTo(".main_menu");
 		$(".characters").css("background-color", "#ffffff");
 		$(".characters").css("color", "#000000");
 		$(".result").empty();
 		$(".reset").hide();
 	}
 
+	greetings();
 	resetCharacters();
 	var gameOver = false;
 	var won = false;
 
 	$(".main_menu").on("click", ".characters" ,function() {
-		// console.log($(this));
 		var characterClicked = $(this).attr("id");
 		var allCharacters = $(".main_menu .characters");
-		// console.log(characterDiv);
-		// $(this).appendTo(".your_character");
-		// if $(".characters").attr() 
 		allCharacters.each(function() {
 			if ($(this).attr("id") != characterClicked) {
 				$(this).appendTo(".enemies_available");
@@ -69,18 +66,18 @@ $(document).ready(function () {
 	})
 
 	$(".enemies_available").on("click", ".characters", function() {
-		// var characterClicked = $(this).attr("id");
-		// console.log(characterClicked);
-		// var allCharacters = $(".enemies_available .characters");
 		if ($(".defender").is(":empty")) {
 			$(this).appendTo(".defender");
 			$(this).css("background-color", "#000000");
 			$(this).css("color", "#ffffff");
+		} else {
+			var defenderName = $(".defender .characters").attr("name");
+			newDiv = $("<p></p>").text("Defeat " + defenderName + " first to move on to another enemy.");
+			$(".result").html(newDiv);
 		}
 	})
 
 	$(".attack").on("click", function() {
-		// alert("You're attacking!");
 		if (!gameOver) {
 			var enemyName = $(".defender .characters").attr("name");
 			var enemyHP = $(".defender .characters").attr("health_points");
@@ -88,44 +85,24 @@ $(document).ready(function () {
 			var hp = $(".your_character .characters").attr("health_points");
 			var damageDone = $(".your_character .characters").attr("attack_power");
 			var damageReceived = $(".defender .characters").attr("counter_attack_power");
-			// console.log("Enemy Name: " + enemyName);
-			// console.log("Enemy HP: " + enemyHP);
-			// console.log("Base Damage Done: " + baseDamageDone);
-			// console.log("Health Points: " + hp);
-			// console.log("Damage Done: " + damageDone);
-			// console.log("Damage Received: " + damageReceived);
-			// should not increase attack power if not attacking enemy!!!!!!!!!1
 			var newAttackPower = parseInt(damageDone) + parseInt(baseDamageDone);
-			// $(".your_character .characters").attr("attack_power", newAttackPower);
 			var newEnemyHP = parseInt(enemyHP) - parseInt(damageDone)
 			$(".defender .characters").attr("health_points", newEnemyHP);
 			$(".defender .characters .hp").text(newEnemyHP);
 			var newHP = parseInt(hp) - parseInt(damageReceived);
-			// console.log("New Attack Power: " + newAttackPower);
-			// console.log("New HP: " + newHP);
-			// console.log("New Enemy HP: " + newEnemyHP);
-			// after attacking one round and resetting, attack button still works but should return "no enemy here"
 			if ($(".defender").is(":empty")) {
-				// $(".your_character .characterClicked").attr("attack_power", damageDone);
 				$(".result").html("<p>No enemy here. Fight all enemies to win the game.</p>");
 			} else if (newEnemyHP <= 0) {
-				// console.log($(".enemies_available").is(":empty"));
 				$(".your_character .characters").attr("attack_power", newAttackPower);
 				if ($(".enemies_available").is(":empty")) {
-					// $(".result").html("<p>You won!!! GAME OVER!!!</p>");
 					gameOver = true;
 					won = true;
-					// $(".defender").empty();
 					$(".defender .characters").appendTo(".graveyard");
 					newDiv = $("<p></p>").text("You won!!! GAME OVER!!!");
 					$(".result").html(newDiv);
-					// check this, doesn't work
-					// add reset button
 					$(".reset").show();
 				} else {
-					// $(".defender").empty();
 					$(".defender .characters").appendTo(".graveyard");
-					// $(".result").html("<p>You have defeated " + enemyName + ". You can choose to fight another enemy.</p>");
 					newDiv = $("<p></p>").text("You have defeated " + enemyName + ". You can choose to fight another enemy.");
 					$(".result").html(newDiv);
 				}
@@ -134,21 +111,17 @@ $(document).ready(function () {
 				$(".your_character .characters").attr("attack_power", newAttackPower);
 				$(".your_character .characters").attr("health_points", newHP);
 				$(".your_character .characters .hp").text(newHP);
-				// $(".result").html("<p>You have been defeated...GAME OVER!!!</p>");
 				newDiv = $("<p></p>").text("You have been defeated...GAME OVER!!!");
 				$(".result").html(newDiv);
 				$(".reset").show();
-				// add reset button
 			} else {
 				$(".your_character .characters").attr("attack_power", newAttackPower);
 				$(".your_character .characters").attr("health_points", newHP);
 				$(".your_character .characters .hp").text(newHP);
-				// syntax may be for HTML, need to use jQuery
 				newDiv1 = $("<p></p>").text("You attacked " + enemyName + " for " + damageDone + " damage.");
 				newDiv2 = $("<p></p>").text(enemyName + " attacked you back for " + damageReceived + " damage.");
 				$(".result").html(newDiv1);
 				newDiv2.appendTo(".result");
-				// $(".result").html("<p>You attacked " + enemyName + " for " + damageDone + " damage.</p><p>" + enemyName + " attacked you back for " + damageReceived + " damage.</p>");
 			}
 		} else {
 			if (!won) {
@@ -162,7 +135,7 @@ $(document).ready(function () {
 	})
 
 	$(".reset").on("click", function() {
-		// alert("you clicked reset");
+		greetings();
 		resetCharacters();
 		resetPlacement();
 	})
